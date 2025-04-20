1b6d6c5aa947c009f838318bd520a6077c694e7c
DESKTOP-0II6FA2\Administrator <jerryjia1999429@Gmail.com>
2025-04-20 21:07:37 +1000

    fixed typo

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -57,7 +57,7 @@ export const useAuthStore = create((set, get) => ({
 
   logout: async () => {
     try {
-      await axiosInstance.post("auth/logout");
+      await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
       get().disconnectSocket();
